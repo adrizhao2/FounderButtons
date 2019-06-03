43d8c4e75fcedbcc48a9ee73663ce75a98c8de6c
Adrianna Zhao <zhaocx@bc.edu>
2019-06-03 14:14:32 -0400

    2.3 FounderButtons

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var messageLabel2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showMessagePressed(_ sender: Any) {
+        messageLabel.text = "Larry Page"
+        messageLabel2.text = "Sergey Brin"
+    }
+    
+    @IBAction func showMessagePressed2(_ sender: Any) {
+        messageLabel.text = "Jennifer Hyman"
+        messageLabel2.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clearMessages(_ sender: Any) {
+        messageLabel.text = ""
+        messageLabel2.text = ""
+    }
 }
 
